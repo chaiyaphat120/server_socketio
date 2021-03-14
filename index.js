c141b822fa12f8688fe92641b167f466c5259e14
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2021-03-14 15:37:09 +0700

    add

--- index.js
+++ index.js
@@ -2,24 +2,14 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
-const port = process.env.PORT || 3000;
-const cors = require("cors")
-app.use(cors())
-app.get('/',(req, res)=>{
-    res.json({
-        message:"connected complate"
-    })
-    
-})
-// io.on("connection", socket => {
-//     console.log("a user connected :D");
-//     socket.on("chat message", ({message , name}) => {
-//         console.log({message , name});
-//         io.emit("chat message", ({message,name}));
-        
-//     });
-    
-// });
+const port = 3000;
 
-server.listen(port, () => console.log("server running on port:" + `${port}`));
-module.exports =  {io}
+io.on("connection", socket => {
+  console.log("a user connected :D");
+  socket.on("chat message", msg => {
+    console.log(msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(port, () => console.log("server running on port:" + port));
