020c87f1a3a839685ba27cd37ee58a4ffcc6515a
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2021-03-14 11:30:48 +0700

    add

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const io = require("socket.io").listen(server);
 const port = 3000;
 
 io.on("connection", socket => {
-  console.log("a user connected :D");
-  socket.on("chat message", msg => {
-    console.log(msg);
-    io.emit("chat message", msg);
-  });
+    console.log("a user connected :D");
+    socket.on("chat message", msg => {
+        console.log(msg);
+        io.emit("chat message", msg);
+    });
 });
 
 server.listen(port, () => console.log("server running on port:" + port));
