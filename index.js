4a5532fe336798a518ce7ed687e0b551ca5ba1cc
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2021-03-14 13:20:57 +0700

    add

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const port = 3000;
 
 io.on("connection", socket => {
     console.log("a user connected :D");
-    socket.on("chat message", msg => {
-        console.log(msg);
-        io.emit("chat message", msg);
+    socket.on("chat message", ({message , name}) => {
+        console.log({message , name});
+        io.emit("chat message", ({message,name}));
     });
 });
 
